6c10f1e17ded2aabe40633c8104a383b396b3adc
Doug <hathemagicpenguin@gmail.com>
2021-02-22 14:57:38 -0500

    Flex/Time-Box/CurrentTime
    Overview 
    Generated filler items for the Time-Boxes, updated items to have correct flex according to the example, set current time as needed.
    
    HTML/CSS
    index.html
    Added all time boxes required.
    Added current css classes and formats to follow example.
    All three items inside each section grow to fill the height of their row.
    Each textarea has a single number as a class to help with JS.
    Each item in the sections show equivalent size to the example.
    Lock emoji is incorrect, using current one as placeholder.
    index.css
    Added nowrap and padding-top: 15px to hour class to fix text adjustment.
    
    Javascript
    index.js
    Added hour variable.
    Added for loop to set current time colors on page.
    
    Repository
    n/a

--- public/index.js
+++ public/index.js
@@ -1,3 +1,19 @@
 const current = $("#currentDay");
+const hour = moment().format("H");
 
-current.text(moment().format('dddd, MMMM D'));
+current.text(moment().format("dddd, MMMM D"));
+
+for (let i = 9; i <= 17; i++) {
+    // console.log("Hello");
+    // console.log(hour);
+    if (hour > i) {
+        // console.log("Past");
+        $(`.${i}`).addClass("past");
+    } else if (hour == i) {
+        // console.log("Present");
+        $(`.${i}`).addClass("present");
+    } else if (hour < i) {
+        // console.log("Future");
+        $(`.${i}`).addClass("future");
+    }
+}
