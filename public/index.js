fb3fa7762a42b09d809de8b7d940f75358caefb7
Doug <hathemagicpenguin@gmail.com>
2021-02-26 11:04:41 -0500

    Data-Value (#2)
    Overview 
    Fixed Data-Value saving feature.
    
    HTML/CSS
    n/a
    
    Javascript
    Fixed Data-Value saving feature.
    
    Repository
    n/a

--- public/index.js
+++ public/index.js
@@ -1,19 +1,31 @@
 const current = $("#currentDay");
 const hour = moment().format("H");
+let x = 0;
+const button = document.querySelectorAll('.saveBtn');
+const textArea = document.querySelectorAll('textarea');
 
 current.text(moment().format("dddd, MMMM D"));
 
 for (let i = 9; i <= 17; i++) {
-    // console.log("Hello");
-    // console.log(hour);
+    const itemClass = $(`.${i}`);
+
     if (hour > i) {
         // console.log("Past");
-        $(`.${i}`).addClass("past");
+        itemClass.addClass("past");
     } else if (hour == i) {
         // console.log("Present");
-        $(`.${i}`).addClass("present");
+        itemClass.addClass("present");
     } else if (hour < i) {
         // console.log("Future");
-        $(`.${i}`).addClass("future");
+        itemClass.addClass("future");
     }
+}
+for (let x = 0; x < button.length; x++) {
+    const buttonItem = button[x];
+    const textItem = textArea[x];
+    textItem.value = localStorage.getItem(`${x}`);
+
+    buttonItem.addEventListener("click", () => {
+        localStorage.setItem(`${x}`, textItem.value);
+    });
 }
